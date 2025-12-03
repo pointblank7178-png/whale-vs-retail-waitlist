a6ddf59b525aa9a41ad9499024bc56ffe7e6e93c
pointblank7178-png <pointblank7178@gmail.com>
2025-12-04 02:02:27 +0700

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ResponseLogger } from "@/components/response-logger";
+import { cookies } from "next/headers";
+import { ReadyNotifier } from "@/components/ready-notifier";
+import FarcasterWrapper from "@/components/FarcasterWrapper";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const requestId = cookies().get("x-request-id")?.value;
+
+  return (
+        <html lang="en">
+          <head>
+            {requestId && <meta name="x-request-id" content={requestId} />}
+          </head>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            {/* Do not remove this component, we use it to notify the parent that the mini-app is ready */}
+            <ReadyNotifier />
+            
+      <FarcasterWrapper>
+        {children}
+      </FarcasterWrapper>
+      
+            <ResponseLogger />
+          </body>
+        </html>
+      );
+}
+
+export const metadata: Metadata = {
+        title: "Whale vs Retail Waitlist",
+        description: "Join the 'Whale vs Retail' trading simulation game waitlist using Farcaster. See recent joiners and their avatars. Admin access with password. Track and manage participants easily.",
+        other: { "fc:frame": JSON.stringify({"version":"next","imageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/thumbnail_663f0945-5b06-4a40-86b8-a44aeebc720b-5mUQIjBqbGPw0n1nNegXd6k3XjNz9N","button":{"title":"Open with Ohara","action":{"type":"launch_frame","name":"Whale vs Retail Waitlist","url":"https://television-combine-108.app.ohara.ai","splashImageUrl":"https://usdozf7pplhxfvrl.public.blob.vercel-storage.com/farcaster/splash_images/splash_image1.svg","splashBackgroundColor":"#ffffff"}}}
+        ) }
+    };
